3345dc777433baee57175bfa6568047fa83a4fa3
Ali Almbaree <engineer.hurby@gmail.com>
2020-08-23 16:41:54 +0300

    Updates

---
+++ precache-manifest.abafb8b02aa233da95cad4ce3fc89bed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e486991a6f3a8b2ab35eb2283504f7",
+    "url": "/smart-news/index.html"
+  },
+  {
+    "revision": "39ed6730a0c6c9480476",
+    "url": "/smart-news/static/css/main.efb7e5c7.chunk.css"
+  },
+  {
+    "revision": "03f54f4f5e298df30a8f",
+    "url": "/smart-news/static/js/2.7aa9e5ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-news/static/js/2.7aa9e5ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ed6730a0c6c9480476",
+    "url": "/smart-news/static/js/main.caafdd3f.chunk.js"
+  },
+  {
+    "revision": "d78b69a522c19e53f20a",
+    "url": "/smart-news/static/js/runtime-main.ebd1bbe8.js"
+  }
+]);
